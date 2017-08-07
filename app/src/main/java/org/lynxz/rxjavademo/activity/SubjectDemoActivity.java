6a10b4174bfa8f3b233ca1fd8e3611df62266aea
lynxz <lynxz8866@gmail.com>
2017-08-07 15:26:42 +0800

    test

---
+++ app/src/main/java/org/lynxz/rxjavademo/activity/SubjectDemoActivity.java
@@ -0,0 +1,18 @@
+package org.lynxz.rxjavademo.activity;
+
+import org.lynxz.rxjavademo.base.BaseActivity;
+
+/**
+ * Created by lynxz on 07/08/2017.
+ */
+public class SubjectDemoActivity extends BaseActivity{
+    @Override
+    public int getLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    public void afterCreate() {
+
+    }
+}
